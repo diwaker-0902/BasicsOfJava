1d51c955006feda245434a2e3d4452c6ce25669b
Diwaker Singh <diwakersinghrj06@gmail.com>
2023-10-29 01:12:59 +0530

    Created 28 Oct 2023

---
+++ helloWorldWithoutSemiColon.java
@@ -0,0 +1,12 @@
+public class helloWorldWithoutSemiColon {
+    public static void main(String[] args){
+        if(System.out.printf("Hello World")==null){
+
+        }
+         
+        
+    }
+}
+// This code will also run if we compare it with not equal to != instead of equal to ==
+// This code is running becoz hello world phle print ho jaega & comparison baad main hoga
+// isliye humko comparison != ya == se farq nahi hoga

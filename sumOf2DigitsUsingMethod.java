012df6587bdebd6e86fcbde5f12457b9c470cc34
Diwaker Singh <diwakersinghrj06@gmail.com>
2023-10-29 02:20:40 +0530

    Created 28 Oct 2023

---
+++ sumOf2DigitsUsingMethod.java
@@ -0,0 +1,14 @@
+public class sumOf2DigitsUsingMethod {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 9;
+        int sum;
+
+        sum = Integer.sum(a, b);
+
+        System.out.println("Sum of two digits : " + sum);
+    }
+
+}
